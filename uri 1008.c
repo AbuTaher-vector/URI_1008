808a1924cce838290861c0fb69614f203a598404
AbuTaher-vector <70890283+AbuTaher-vector@users.noreply.github.com>
2020-09-10 12:10:05 +0600

    Add files via upload

---
+++ uri 1008.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main()
+{
+   int NUMBER,A;
+    double B,SALARY;
+    scanf("%d %d %lf",&NUMBER,&A,&B);
+   SALARY=A*B;
+   printf("NUMBER = %d\nSALARY = U$ %.2lf\n",NUMBER,SALARY);
+    return 0;
+ }
